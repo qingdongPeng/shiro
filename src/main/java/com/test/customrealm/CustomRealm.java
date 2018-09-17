86de9b0ccc5ae408a356fc927438d3a32733a9e3
qingdongPeng <38778076+qingdongPeng@users.noreply.github.com>
2018-09-17 21:12:59 +0800

    自定义realm及加密

--- src/main/java/com/test/customrealm/CustomRealm.java
+++ src/main/java/com/test/customrealm/CustomRealm.java
@@ -5,11 +5,14 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Description:自定义Realm，继承AuthorizingRealm
@@ -23,7 +26,8 @@ public class CustomRealm extends AuthorizingRealm
     Map<String, String> userMap = new HashMap<String, String>();
 
     {
-        userMap.put("pqd", "123456");
+        //密码采用的是MD5加密过,原密码为123456
+        userMap.put("pqd", "e10adc3949ba59abbe56e057f20f883e");
         //设置realmName
         super.setName("custom");
     }
@@ -31,8 +35,20 @@ public class CustomRealm extends AuthorizingRealm
     //授权
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection)
     {
-        return null;
+        //从主体传过来的认证信息中，获取用户名
+        String username = (String) principalCollection.getPrimaryPrincipal();
+        //从数据库或缓存中获取角色数据
+        Set<String> roles = getRolesByUserName(username);
+        //从数据库或缓存中获取角色权限
+        Set<String> permissons = getPermissionByUserName(username);
+        //构建返回对象
+        SimpleAuthorizationInfo simpleAuthenticationInfo = new SimpleAuthorizationInfo();
+        simpleAuthenticationInfo.setStringPermissions(permissons);
+        simpleAuthenticationInfo.setRoles(roles);
+        return simpleAuthenticationInfo;
     }
+
+
 
     //认证
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken)
@@ -50,12 +66,31 @@ public class CustomRealm extends AuthorizingRealm
         }
 
         //创建返回对象
-        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo("pqd",password,"custom");
-        return simpleAuthenticationInfo;
+        return new SimpleAuthenticationInfo("pqd",password,"custom");
     }
 
+    /**
+     * 模拟用户
+     */
     private String getPasswordByName(String username)
     {
         return userMap.get(username);
+    }
+
+    /**
+     * 模拟角色数据
+     */
+    private Set<String> getRolesByUserName(String username) {
+        Set <String> set = new HashSet<String>();
+        set.add("admin");
+        set.add("user");
+        return  set;
+    }
+
+    private Set<String> getPermissionByUserName(String username) {
+        Set <String> set = new HashSet<String>();
+        set.add("user:delete");
+        set.add("user:add");
+        return set;
     }
 }
--- src/main/java/com/test/customrealm/CustomRealmTest.java
+++ src/main/java/com/test/customrealm/CustomRealmTest.java
@@ -2,6 +2,8 @@ package com.test.customrealm;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.junit.Test;
@@ -13,11 +15,10 @@ import org.junit.Test;
  * @author pengqd
  * @Date 2018/9/17 14:09
  */
-public class CustomRealmTest
-{
+public class CustomRealmTest {
 
     @Test
-    public void TestCustomRealm(){
+    public void TestCustomRealm() {
 
         CustomRealm customRealm = new CustomRealm();
 
@@ -25,15 +26,28 @@ public class CustomRealmTest
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
         securityManager.setRealm(customRealm);
 
+        //对数据进行加密
+        HashedCredentialsMatcher matcher = new HashedCredentialsMatcher();
+        matcher.setHashAlgorithmName("md5"); //采用md5加密
+        matcher.setHashIterations(1);        //设置加密次数
+        customRealm.setCredentialsMatcher(matcher);
+
         //主体提交请求认证
         SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
 
-        UsernamePasswordToken token = new UsernamePasswordToken("pqd","123456");
+        UsernamePasswordToken token = new UsernamePasswordToken("pqd", "123456");
         subject.login(token);
 
         //判断是否认证
         System.out.println(subject.isAuthenticated());
+
+        //授权测试
+        subject.checkRoles("admin","user");
+        subject.checkPermissions("user:delete","user:add");
+
+        /*Md5Hash hash = new Md5Hash("123456");
+        System.out.println(hash);*/
 
     }
 }
